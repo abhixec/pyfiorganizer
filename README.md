41927e8e8409582b5f032b60f08ada516f427d67
Abhinav <abhixec@gmail.com>
2020-08-22 16:10:58 -0700

    Add pyfiorganizer, Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pyfiorganizer
+# PYthonFIleORGANIZER
+
+PYthonFIleORGANIZER (a.k.a pyfiorganizer), is a simple python script to unclutter my `~/Downloads` folder. 
+
+Currently it creates `images`, `docs`, `audio`, `videos` and `misc` directories if they don't already exist. 
+
+## TOOD 
+- [ ] Read configuration file from user config directory to better move user files to directory of liking.
+- [ ] Add more file types
+- [ ] Create an indexer file which can be used to lookup where each file went for searchability.
+
---
+++ pyfiorganizer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Author : Abhinav
+
+import os
+from os import path
+import shutil
+import sys
+
+class Organizer:
+
+    def __init__(self, dir):
+        self.dir = dir
+        self.directories = ['images','docs','audio','videos','misc']
+        self.directory_association = {
+            'images': ['jpg','png','svg','jpeg']
+            , 'docs': ['pdf', 'odt', 'doc', 'docx', 'xls']
+            , 'audio': ['mp3']
+            , 'videos': ['mp4']
+        }
+        
+
+    def setup(self):
+        print("Changing to dir %s"%(self.dir))
+        os.chdir(self.dir)
+        for directory in self.directories:
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+
+
+    def move(self):
+        print("Moving all the files into the respective directory")
+        with os.scandir() as it:
+            for entry in it:
+                if entry.is_file():
+                    filename, ext = path.splitext(entry)
+                    print('Moving %s with extensinon %s'%(filename, ext) )
+                    for k,v in self.directory_association.items():
+                        if ext.replace('.','') in v:
+                            print(ext, v, entry.name)
+                            shutil.move(entry.name, k)
+                            break
+                        else:
+                            shutil.move(entry.name, 'misc')
+                            break
+                        
+
+
+if __name__=='__main__':
+    directory_name = sys.argv[1]
+    organizer = Organizer(directory_name)
+    organizer.setup()
+    organizer.move()
